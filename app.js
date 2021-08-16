302f6fdc0e9b67210401bd04fc55f466ee5260c3
Hamza91able <m.hamzakhan91able@gmail.com>
2021-08-17 00:16:41 +0500

    Changes

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 "use strict";
 
+require('dotenv').config()
 const path = require("path");
 const AutoLoad = require("fastify-autoload");
 const connectDB = require("./config/db");
 
-module.exports = async function (fastify, opts) {
-  // Place here your custom code!
+const fastify = require("fastify")({
+  logger: true,
+});
+
+fastify.register(AutoLoad, {
+  dir: path.join(__dirname, "plugins"),
+});
+
+// This loads all plugins defined in routes
+// define your routes in one of these
+fastify.register(AutoLoad, {
+  dir: path.join(__dirname, "routes"),
+});
+
+fastify.listen(process.env.PORT, function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
   connectDB();
-
-  // Do not touch the following lines
-
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, "plugins"),
-    options: Object.assign({}, opts),
-  });
-
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, "routes"),
-    options: Object.assign({}, opts),
-  });
-};
+});
