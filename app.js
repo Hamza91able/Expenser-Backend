5948edd9a718966f7bdcc6147f35ef1b9beae3bd
Hamza91able <m.hamzakhan91able@gmail.com>
2021-08-17 00:57:04 +0500

    test

--- app.js
+++ app.js
@@ -21,22 +21,10 @@ fastify.register(AutoLoad, {
   dir: path.join(__dirname, "routes"),
 });
 
-(async () => {
-  console.log("IN HERE");
-  console.log();
-  const ip = await publicIp.v4();
-  fastify.listen(
-    process.env.PORT,
-    ip || process.env.hostname,
-    function (err, address) {
-      if (err) {
-        fastify.log.error(err);
-        process.exit(1);
-      }
-      connectDB();
-    }
-  );
-  //=> '46.5.21.123'
-
-  //=> 'fe80::200:f8ff:fe21:67cf'
-})();
+fastify.listen(process.env.PORT, process.env.hostname, function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  connectDB();
+});
